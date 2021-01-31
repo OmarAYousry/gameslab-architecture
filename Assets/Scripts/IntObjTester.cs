e32594e54531d656fcfe5fe871007bbf626538e3
Tarek ElSherif <tarek.elsherif9@gmail.com>
2021-01-31 13:05:50 +0100

    Added test script

---
+++ Assets/Scripts/IntObjTester.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntObjTester : MonoBehaviour
+{
+    private void Start()
+    {
+        InteractableObject.interactables[0].Test();
+    }
+}
