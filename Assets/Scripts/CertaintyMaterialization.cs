dbf2b0378583acb4c2a5eb447d1102adea5fb6f7
Omar Ahmed <omar.ahmed@tum.de>
2021-02-01 06:55:47 +0100

    [feature] Add CertaintyMaterialization class that handles previewing the certainty through different materials
    TODO: Allow class to be self-contained. Currently InteractableObject handles too much of the internal logic

---
+++ Assets/Scripts/CertaintyMaterialization.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CertaintyMaterialization : MonoBehaviour
+{
+    private static CertaintyMaterialization instance = null;
+
+    [Tooltip("Sort Ascendingly")]
+    [SerializeField]
+    private Material[] certaintyMatsSorted = null;
+
+    public static Material[] CertaintyMats { get { return instance.certaintyMatsSorted; } }
+
+    public static bool isPreviewCertainty { get; private set; } = false;
+
+    void Awake()
+    {
+        instance = this;        
+    }
+
+    public void ToggleCertaintyMats(bool applyingCertainty)
+    {
+        isPreviewCertainty = applyingCertainty;
+
+        foreach (InteractableObject interObj in InteractableObject.interactables)
+        {
+            interObj.ToggleCertaintyMaterial(applyingCertainty, certaintyMatsSorted);
+        }
+    }
+}
