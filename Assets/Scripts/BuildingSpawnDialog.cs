0e6511bba594852fb5ba4e99ca8c9d26b2a7b1c5
Omar Ahmed <omar.ahmed@tum.de>
2021-04-05 07:51:09 +0200

    [feature] add building selection UI when user taps on the AR grid to spawn a model
    TODO: Adjust UI to be more in-line with other UI elements; check building scale; more testing

---
+++ Assets/Scripts/BuildingSpawnDialog.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingSpawnDialog : MonoBehaviour
+{
+    private static BuildingSpawnDialog instance = null;
+
+    [SerializeField]
+    private UnityEngine.UI.Dropdown buildingDropdown;
+
+    [SerializeField]
+    private UnityEngine.UI.Button confirmButton;
+
+    private static System.Action<GameObject> spawnAction = null;
+
+    private static int currentBuildingIndex = 0;
+
+    private static List<GameObject> spawnableBuildings = null;
+
+    private void Awake()
+    {
+        instance = this;
+        Debug.LogError("Wake me up lul");
+        gameObject.SetActive(false);
+    }
+
+    public void UpdateChosenBuildingIndex(int newBuildingIndex)
+    {
+        currentBuildingIndex = newBuildingIndex;
+    }
+
+    public void OnConfirmButtonClicked()
+    {
+        string currentBuildingName = instance.buildingDropdown.options[currentBuildingIndex].text;
+        GameObject buildingToSpawn = spawnableBuildings.Find(building => building.name == currentBuildingName);
+        spawnAction(buildingToSpawn);
+        instance.gameObject.SetActive(false);
+        //instance.Invoke("disableMe", 3f);
+    }
+
+    private void disableMe()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public static void DisplayBuildingSpawnDialog(List<GameObject> spawnableBuildings, System.Action<GameObject> spawnAction)
+    {
+        BuildingSpawnDialog.spawnableBuildings = spawnableBuildings;
+        BuildingSpawnDialog.spawnAction = spawnAction;
+
+        List<string> spawnableBuildingNames = new List<string>();
+        foreach (GameObject building in spawnableBuildings)
+        {
+            spawnableBuildingNames.Add(building.name);
+        }
+
+        instance.buildingDropdown.ClearOptions();
+        instance.buildingDropdown.AddOptions(spawnableBuildingNames);
+        ////instance.buildingDropdown.onValueChanged = UpdateChosenBuildingIndex;
+        //UnityEngine.UI.Dropdown.DropdownEvent d = new UnityEngine.UI.Dropdown.DropdownEvent();
+        //d.AddListener(UpdateChosenBuildingIndex);
+        //currentBuildingIndex = instance.buildingDropdown.optio
+
+        instance.gameObject.SetActive(true);
+
+    }
+
+    public static bool isActive()
+    {
+        return instance.gameObject.activeInHierarchy;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Touch/PlaceBuildingOnPlane.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+/// <summary>
+/// Listens for touch events and performs an AR raycast from the screen touch point.
+/// AR raycasts will only hit detected trackables like feature points and planes.
+///
+/// If a raycast hits a trackable, the <see cref="placedPrefabs"/> is instantiated
+/// and moved to the hit position.
+/// </summary>
+[RequireComponent(typeof(ARRaycastManager))]
+public class PlaceBuildingOnPlane : MonoBehaviour
+{
+    [SerializeField]
+    [Tooltip("Instantiates the prefab from the list on a plane at the touch location.")]
+    List<GameObject> m_PlacedPrefabs = new List<GameObject>();
+
+    bool m_spawned = false;
+
+    /// <summary>
+    /// The prefab to instantiate on touch.
+    /// </summary>
+    public List<GameObject> placedPrefabs
+    {
+        get { return m_PlacedPrefabs; }
+        set { m_PlacedPrefabs = value; }
+    }
+
+    /// <summary>
+    /// The object instantiated as a result of a successful raycast intersection with a plane.
+    /// </summary>
+    public GameObject spawnedObject { get; private set; }
+
+    void Awake()
+    {
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+#if UNITY_EDITOR
+        if (Input.GetMouseButton(0))
+        {
+            var mousePosition = Input.mousePosition;
+            touchPosition = new Vector2(mousePosition.x, mousePosition.y);
+            return true;
+        }
+#else
+        if (Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+#endif
+
+        touchPosition = default;
+        return false;
+    }
+
+    Pose hitPose = default;
+
+    void Update()
+    {
+        // only spawn the building once (through this script at least)
+        if (m_spawned)
+            return;
+        if (BuildingSpawnDialog.isActive())
+            return;
+        if (!TryGetTouchPosition(out Vector2 touchPosition))
+            return;
+
+        if (m_RaycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            // Raycast hits are sorted by distance, so the first one
+            // will be the closest hit.
+            //var hitPose = s_Hits[0].pose;
+            hitPose = s_Hits[0].pose;
+
+            if (spawnedObject == null)
+            {
+                BuildingSpawnDialog.DisplayBuildingSpawnDialog(m_PlacedPrefabs, setAsSpawned);
+                //spawnedObject = Instantiate(m_PlacedPrefabs, hitPose.position, hitPose.rotation);
+            }
+            //else
+            //{
+            //    spawnedObject.transform.position = hitPose.position;
+            //}
+        }
+    }
+
+    private void setAsSpawned(GameObject spawnedObject)
+    {
+        spawnedObject = Instantiate(spawnedObject, hitPose.position, hitPose.rotation);
+        this.spawnedObject = spawnedObject;
+        m_spawned = true;
+    }
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    ARRaycastManager m_RaycastManager;
+}
